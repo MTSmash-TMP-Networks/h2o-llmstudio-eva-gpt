bbcf0e4e2b9ad32ab0a228d901889ec22051e6a2
Marek Templin <76729306+MTSmash-TMP-Networks@users.noreply.github.com>
2026-07-31 10:56:34 +0200

    Enable fast long-sample indexing

---
+++ llm_studio/src/datasets/__init__.py
@@ -0,0 +1,6 @@
+"""Dataset package initialization."""
+
+from llm_studio.src.datasets.fast_sliding_window import install_fast_sliding_window
+
+install_fast_sliding_window()
+del install_fast_sliding_window
