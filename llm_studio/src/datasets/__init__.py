eef6a57c547dc6d3681ecfa21a3abf4e2bc78f12
Marek Templin <76729306+MTSmash-TMP-Networks@users.noreply.github.com>
2026-08-03 12:17:25 +0200

    Enable structure-aware sliding windows

--- llm_studio/src/datasets/__init__.py
+++ llm_studio/src/datasets/__init__.py
@@ -4,3 +4,10 @@ from llm_studio.src.datasets.fast_sliding_window import install_fast_sliding_win
 
 install_fast_sliding_window()
 del install_fast_sliding_window
+
+from llm_studio.src.datasets.structure_aware_sliding_window import (
+    install_structure_aware_sliding_window,
+)
+
+install_structure_aware_sliding_window()
+del install_structure_aware_sliding_window
