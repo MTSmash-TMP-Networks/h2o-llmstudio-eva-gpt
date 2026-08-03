32a41808178fa367a8d783281d0e20df539980ae
Marek Templin <76729306+MTSmash-TMP-Networks@users.noreply.github.com>
2026-08-03 12:19:31 +0200

    Accept structure-aware fast indexer subclass

--- tests/src/datasets/test_fast_sliding_window_import.py
+++ tests/src/datasets/test_fast_sliding_window_import.py
@@ -1,5 +1,9 @@
 def test_dataset_package_import_installs_fast_indexer():
     from llm_studio.src.datasets.fast_sliding_window import FastSlidingWindowDataset
+    from llm_studio.src.datasets.structure_aware_sliding_window import (
+        StructureAwareSlidingWindowDataset,
+    )
     from llm_studio.src.datasets.text_causal_language_modeling_ds import CustomDataset
 
-    assert CustomDataset is FastSlidingWindowDataset
+    assert issubclass(CustomDataset, FastSlidingWindowDataset)
+    assert CustomDataset is StructureAwareSlidingWindowDataset
