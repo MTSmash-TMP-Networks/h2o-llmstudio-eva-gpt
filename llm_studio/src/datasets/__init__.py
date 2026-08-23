96138e92cf5339a3a027a92391e2c6ba99b30487
Marek Templin <76729306+MTSmash-TMP-Networks@users.noreply.github.com>
2026-08-23 11:13:53 +0200

    Install supervised answer validation

--- llm_studio/src/datasets/__init__.py
+++ llm_studio/src/datasets/__init__.py
@@ -4,9 +4,14 @@ from llm_studio.src.datasets.fast_sliding_window import install_fast_sliding_win
 from llm_studio.src.datasets.structure_aware_sliding_window import (
     install_structure_aware_sliding_window,
 )
+from llm_studio.src.datasets.supervised_answer_validation import (
+    install_supervised_answer_validation,
+)
 
 install_fast_sliding_window()
 install_structure_aware_sliding_window()
+install_supervised_answer_validation()
 
 del install_fast_sliding_window
 del install_structure_aware_sliding_window
+del install_supervised_answer_validation
