a695a7386e6ac647387b73d2bf7dfb73e916a909
Marek Templin <76729306+MTSmash-TMP-Networks@users.noreply.github.com>
2026-08-24 13:07:31 +0200

    Install context-only chain turn support

--- llm_studio/src/datasets/__init__.py
+++ llm_studio/src/datasets/__init__.py
@@ -1,5 +1,8 @@
 """Dataset package initialization."""
 
+from llm_studio.src.datasets.context_only_chain_turns import (
+    install_context_only_chain_turns,
+)
 from llm_studio.src.datasets.exact_chained_training import (
     install_exact_chained_training,
 )
@@ -13,9 +16,11 @@ from llm_studio.src.datasets.supervised_answer_validation import (
 
 install_fast_sliding_window()
 install_structure_aware_sliding_window()
+install_context_only_chain_turns()
 install_supervised_answer_validation()
 install_exact_chained_training()
 
+del install_context_only_chain_turns
 del install_exact_chained_training
 del install_fast_sliding_window
 del install_structure_aware_sliding_window
